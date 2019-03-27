b121e25562886df6a449381a096a7688e5266bdb
chague <Collin@Dragonborn-Mobile.localdomain>
2019-03-27 14:08:27 -0400

    created some of the files and started ExperimentManager.cpp and ExperimentManager.h

---
+++ AllYouCanHeat/ExperimentManager.cpp
@@ -0,0 +1,22 @@
+#include "HeatPad.h"
+#include "LCD.h"
+#include "LoadCell.h"
+#include "Motor.h"
+#include "Thermistor.h"
+using namespace Heat;
+
+class Manager {
+  int state = 0;
+  HeatPad heatPad;
+  LCD lcd;
+  LoadCell loadcell;
+  Motor motor;
+  Thermistor thermistor0;
+  Thermistor thermistor1;
+
+  void setup(void) {
+  }
+  void loop(void) {
+  }
+}
+
---
+++ AllYouCanHeat/ExperimentManager.h
@@ -0,0 +1,13 @@
+/*
+ * @author Collin Hague
+ * ExperimentManager.h
+ * header for ExperimentManager.cpp
+ */
+ namespace heat {
+  class Manager {
+    public:
+      void setup(void);
+      void loop(void);
+  }
+ }
+
