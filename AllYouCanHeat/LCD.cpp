f39fa5b127ae3e6cb6464495f2bf8b35cefe905c
chague <Collin@Dragonborn-Mobile.localdomain>
2019-04-03 12:51:43 -0400

    created lcd testfile for lcd experimentation and started the lcd file

--- AllYouCanHeat/LCD.cpp
+++ AllYouCanHeat/LCD.cpp
@@ -12,7 +12,17 @@ When a button is pressed, the backlight changes color.
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
 
+using namespace heat;
 
+LCD::LCD {
+  this->sheild = lcd AdafruitRGBLCDShield();
+}
+
+LCD::setup() {
+  this->lcd.begin( 16, 2 );
+  this->lcd.print( "All You Can Heat" )
+  this->lcd.setBacklight( ON );
+}
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
 // this is Analog 4 and 5 so you can't use those for analogRead() anymore
 // However, you can connect other I2C sensors to the I2C bus and share
